682fc0494ec1326d2efdddb8468bb72b66f177bc
Anthony Bauld <102757875+AnthonyBauld@users.noreply.github.com>
2022-08-25 10:57:19 -0400

    Create FileCharacter.java

---
+++ Assignment 3/FileCharacter.java
@@ -0,0 +1,46 @@
+import java.util.ArrayList;
+
+/**
+ * I Anthony Bauld, 000754334 certify that this material is my original work. No other person's work has been used without due acknowledgement. 
+ * I have not made my work available to anyone else.
+ * @author Anthony Bauld - 000754334
+ */
+public class FileCharacter implements Index {
+    private final String name;
+    private int appearanceCount;
+    private int closenessCount;
+    private double closenessFactor;
+    private ArrayList < Integer > positions = new ArrayList < > ();
+
+    public FileCharacter(String name) {
+        this.name = name;
+        this.appearanceCount = 0;
+        this.closenessCount = 0;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void incrementClosenessCount() {
+        closenessCount++;
+        closenessFactor = (double) closenessCount / appearanceCount;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%-10s, %d] is close to the Ring %d times with a closeness factor of %5.4f",
+                name, appearanceCount, closenessCount, closenessFactor);
+    }
+
+    @Override
+    public void addPosition(int position) {
+        positions.add(position);
+        appearanceCount++;
+    }
+
+    @Override
+    public ArrayList < Integer > getPositions() {
+        return positions;
+    }
+}
