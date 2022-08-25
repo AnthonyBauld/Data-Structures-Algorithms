8a1fc478ee513c64fd5154faf6f853cda1759af4
Anthony Bauld <102757875+AnthonyBauld@users.noreply.github.com>
2022-08-25 10:59:01 -0400

    Update Ring.java

--- Assignment 3/Ring.java
+++ Assignment 3/Ring.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 
 /**
- * I Anthony Bauld, 000754334 certify that this material is my original work. No other person's work has been used without due acknowledgement. 
+ * I Anthony Bauld, certify that this material is my original work. No other person's work has been used without due acknowledgement. 
  * I have not made my work available to anyone else.
- * @author Anthony Bauld - 000754334
+ *
+ * @author Anthony Bauld 
  */
 public class Ring implements Index {
     private final ArrayList < Integer > positions = new ArrayList < > ();
