76bfaca2e37bd798a717ccd962d072c7d1deffd6
Anthony Bauld <102757875+AnthonyBauld@users.noreply.github.com>
2022-08-25 10:59:42 -0400

    Update Timer.java

--- Assignment 2/Timer.java
+++ Assignment 2/Timer.java
@@ -1,7 +1,8 @@
 /**
- * I Anthony Bauld, 000754334 certify that this material is my original work. No other person's work has been used without due acknowledgement. 
+ * I Anthony Bauld, certify that this material is my original work. No other person's work has been used without due acknowledgement. 
  * I have not made my work available to anyone else.
- * @author Anthony Bauld - 000754334
+ *
+ * @author Anthony Bauld 
  */
 public class Timer {
     int count;
