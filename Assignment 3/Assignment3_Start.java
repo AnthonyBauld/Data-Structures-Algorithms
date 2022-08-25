a751499bf17cf40d60f7d9612d20f896f2abb653
Anthony Bauld <102757875+AnthonyBauld@users.noreply.github.com>
2022-08-25 10:59:33 -0400

    Update Assignment3_Start.java

--- Assignment 3/Assignment3_Start.java
+++ Assignment 3/Assignment3_Start.java
@@ -21,8 +21,10 @@ import java.util.Comparator;
 import java.util.Scanner;
 
 /**
- * I Anthony Bauld, 000754334 certify that this material is my original work. No other person's work has been used without due acknowledgement. 
+ * I Anthony Bauld, certify that this material is my original work. No other person's work has been used without due acknowledgement. 
  * I have not made my work available to anyone else.
+ *
+ * @author Anthony Bauld 
  */
 public class Assignment3_Start {
     /**
