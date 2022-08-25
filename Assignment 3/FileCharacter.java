cb0c10b29b949737919d03e360db5de1523c8281
Anthony Bauld <102757875+AnthonyBauld@users.noreply.github.com>
2022-08-25 10:59:08 -0400

    Update FileCharacter.java

--- Assignment 3/FileCharacter.java
+++ Assignment 3/FileCharacter.java
@@ -1,9 +1,10 @@
 import java.util.ArrayList;
 
 /**
- * I Anthony Bauld, 000754334 certify that this material is my original work. No other person's work has been used without due acknowledgement. 
+ * I Anthony Bauld, certify that this material is my original work. No other person's work has been used without due acknowledgement. 
  * I have not made my work available to anyone else.
- * @author Anthony Bauld - 000754334
+ *
+ * @author Anthony Bauld 
  */
 public class FileCharacter implements Index {
     private final String name;
