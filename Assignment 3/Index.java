5b18eb83e3df863fd4552e4ef3fe23cc72fed21f
Anthony Bauld <102757875+AnthonyBauld@users.noreply.github.com>
2022-08-25 10:57:31 -0400

    Create Index.java

---
+++ Assignment 3/Index.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * I Anthony Bauld, 000754334 certify that this material is my original work. No other person's work has been used without due acknowledgement. 
+ * I have not made my work available to anyone else.
+ * @author Anthony Bauld - 000754334
+ */
+interface Index {
+    void addPosition(int position);
+
+    ArrayList < Integer > getPositions();
+}
